4564351d054d7efabe318adb0288cd127ae12185
Albert P <albert1742@gmail.com>
2023-01-04 12:13:49 +0100

    ProductController and CartController endpoint implementation

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,32 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.dto.CartDto;
+import com.kodilla.ecommercee.domain.dto.ProductDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/cart")
 public class CartController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void cartCreation(@RequestBody ProductDto productDto){
+
+    }
+    @GetMapping(value = "{cartId}")
+    public CartDto getCartElements(@PathVariable Long cartId){
+        return null;
+    }
+    @PutMapping(value ="add")
+    public void addProductToCart(@RequestParam Long productId){
+
+    }
+    @PutMapping(value ="delete")
+    public void deleteProductFromCard(@RequestParam Long productId){
+
+    }
+    @PutMapping(value ="order")
+    public void makeOrderFromCart(){
+
+    }
 }
