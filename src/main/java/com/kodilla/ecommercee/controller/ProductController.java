63ee2b2a8d3766e13bcccca6218db7ddaa84e0fe
Albert P <albert1742@gmail.com>
2023-01-05 18:34:37 +0100

    JDP230101-1

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.dto.ProductDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("v1/product")
 public class ProductController {
+
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{productId}")
+    public ProductDto getProduct(@PathVariable Long productId) {
+        return new ProductDto();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createProduct(@RequestBody ProductDto productDto) {
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
+        return new ProductDto();
+    }
+
+
+    @DeleteMapping(value = "{productId}")
+    public void deleteProduct(@PathVariable Long productId) {
+    }
 }
