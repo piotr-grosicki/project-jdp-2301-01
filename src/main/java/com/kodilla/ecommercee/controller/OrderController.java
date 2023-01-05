5408187bc088d6c284c7cc3f3fc2da7581e26644
pietryczko <110178231+pietryczko@users.noreply.github.com>
2023-01-05 12:50:04 +0100

    Branch split: UserController and UserDto implementation

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,38 +1,5 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.dto.OrderDto;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-@RequestMapping("v1/order")
 public class OrderController {
 
-    @GetMapping
-    public List<OrderDto> getOrder() {
-        return new ArrayList<>();
-    }
-
-    @GetMapping(value = "{orderId}")
-    public OrderDto getOrder(@PathVariable Long orderId) {
-        return new OrderDto();
-    }
-
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createOrder(@RequestBody OrderDto orderDto) {
-    }
-
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
-        return new OrderDto();
-    }
-
-
-    @DeleteMapping(value = "{orderId}")
-    public void deleteOrder(@PathVariable Long orderId) {
-    }
 }
-
