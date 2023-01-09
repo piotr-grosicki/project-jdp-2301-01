c22b6972b4f450d0da69126023053b0c562d55db
Tomasz Kowalski <tomek.kowalski.89@gmail.com>
2023-01-09 23:03:53 +0100

    Jdp230101_13

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,24 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCTS")
+public class Product {
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "PRODUCT_ID", unique = true)
+    private Long productId;
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID")
+    private Group group;
+}
