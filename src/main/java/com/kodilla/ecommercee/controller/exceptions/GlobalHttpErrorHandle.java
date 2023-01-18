6414331860bd5e677014059a05f15200bd5cb5bd
Albert P <albert1742@gmail.com>
2023-01-18 12:56:55 +0100

    JDP230101-22
    JDP230101-22
    
    JDP230101-22
    
     JDP230101-22

---
+++ src/main/java/com/kodilla/ecommercee/controller/exceptions/GlobalHttpErrorHandle.java
@@ -0,0 +1,15 @@
+package com.kodilla.ecommercee.controller.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHttpErrorHandle extends ResponseEntityExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Object> handleNotFoundException(GroupNotFoundException exception){
+        return new ResponseEntity<>("Requested data doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/kodilla/ecommercee/service/GroupDbService.java
+++ src/main/java/com/kodilla/ecommercee/service/GroupDbService.java
@@ -1,12 +1,33 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.controller.exceptions.GroupNotFoundException;
+import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class GroupDbService {
 
-    private GroupRepository groupRepository;
+    private final GroupRepository groupRepository;
+
+    public List<Group> getAllGroups() {
+        return groupRepository.findAll();
+    }
+
+    public Group getGroup(final Long groupId) throws GroupNotFoundException {
+        return groupRepository.findById(groupId).orElseThrow(GroupNotFoundException::new);
+    }
+
+    public Group saveGroup(final Group group) {
+        return groupRepository.save(group);
+    }
+
+    public void deleteGroup(final Long groupId) {
+        groupRepository.deleteById(groupId);
+    }
 }
+
